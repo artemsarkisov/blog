92fd2420247fc673b7e76541744840aab6343d9d
Artem Sarkisov <artem.sarkisov@jetbrains.com>
2019-03-21 19:31:00 +0300

    Add a temporary fix that leaves some comments in files which normally should never be done. This commit should be reverted and fixed in an adequate manner.

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,35 +1,37 @@
-# spec/factories/articles.rb
+#spec/factories/articles.rb
 
-FactoryBot.define do
+# FactoryBot.define do
 
   # post factory with a `belongs_to` association for the user
-  factory :article do
-    title { "Through the Looking Glass" }
-    user
-  end
+  # factory :article do
+  #   title { "Through the Looking Glass" }
+  #   user
+  # end
+
+  # THE line below will create a mess if you uncomment it
 
   # user factory without associated articles
-  factory :user do
-    name { "John Doe" }
-
-    # user_with_posts will create post data after the user has been created
-    factory :user_with_posts do
-      # articles_count is declared as a transient attribute and available in
-      # attributes on the factory, as well as the callback via the evaluator
-      transient do
-        articles_count { 5 }
-      end
-
-      # the after(:create) yields two values; the user instance itself and the
-      # evaluator, which stores all values from the factory, including transient
-      # attributes; `create_list`'s second argument is the number of records
-      # to create and we make sure the user is associated properly to the post
-      after(:create) do |user, evaluator|
-        create_list(:article, evaluator.articles_count, user: user)
-      end
-    end
-  end
-end
+  # factory :user do
+  #   name { "John Doe" }
+  #
+  #   # user_with_posts will create post data after the user has been created
+  #   factory :user_with_posts do
+  #     # articles_count is declared as a transient attribute and available in
+  #     # attributes on the factory, as well as the callback via the evaluator
+  #     transient do
+  #       articles_count { 5 }
+  #     end
+  #
+  #     # the after(:create) yields two values; the user instance itself and the
+  #     # evaluator, which stores all values from the factory, including transient
+  #     # attributes; `create_list`'s second argument is the number of records
+  #     # to create and we make sure the user is associated properly to the post
+  #     after(:create) do |user, evaluator|
+  #       create_list(:article, evaluator.articles_count, user: user)
+  #     end
+  #   end
+  # end
+# end
 
 # FactoryBot.define do
 #   factory :article do
@@ -43,12 +45,12 @@ end
 #       status :unpublished
 #     end
 #
-#     trait :in_the_future do
-#       published_at { 2.days.from_now }
-#     end
+#     # trait :in_the_future do
+#     #   published_at { 2.days.from_now }
+#     # end
 #
-#     trait :in_the_past do
-#       published_at { 2.days.ago }
-#     end
+#     # trait :in_the_past do
+#     #   published_at { 2.days.ago }
+#     # end
 #   end
 # end
